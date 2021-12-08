c22bd1164a57cdef0fe7f6659b1d4472fec3e596
welder <welder@supermercadosbh.com.br>
2021-12-08 17:23:52 -0300

    Refatoração

--- ApiTelefone/Controllers/TelefonesController.cs
+++ ApiTelefone/Controllers/TelefonesController.cs
@@ -1,104 +1,106 @@
 
 
 using System.Collections.Generic;
+using System.Linq;
 using ApiTelefone.Models;
 using ApiTelefone.Repositorio;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTelefone.Controllers
 {
-	[Route("api/[Controller]")]
-	public class TelefonesController : Controller
-	{
-		private readonly ITelefonesRepository _telefoneRepositorio;
+    [Route("api/[Controller]")]
+    public class TelefonesController : Controller
+    {
+        private readonly ITelefonesRepository _telefoneRepositorio;
 
-		public TelefonesController(ITelefonesRepository telefoneRepo)
-		{
-			_telefoneRepositorio = telefoneRepo;
-		}
+        public TelefonesController(ITelefonesRepository telefoneRepo)
+        {
+            _telefoneRepositorio = telefoneRepo;
+        }
 
-		[HttpGet]
-		public IEnumerable<Telefones> GetAll()
-		{
-			var listaTelefones = _telefoneRepositorio.GetAll();
+        [HttpGet]
+        public List<Telefones> GetAll()
+        {
+            var listaTelefones = _telefoneRepositorio.GetAll().ToList();
 
-			return listaTelefones;
-		}
+            return listaTelefones;
+        }
 
-		[HttpGet("{id}", Name = "GetTelefone")]
-		public IActionResult GetById(long id)
-		{
-			var loja = _telefoneRepositorio.Find(id);
-			if (loja == null)
-				return NotFound();//Retornar erro404
+        [HttpGet("{id}", Name = "GetTelefone")]
+        public IActionResult GetById(int id)
+        {
+            var loja = _telefoneRepositorio.Find(id);
+            if (loja == null)
+                return NotFound();//Retornar erro404
 
-			return new ObjectResult(loja);//retorna codigo 200 OK
-		}
+            return new ObjectResult(loja);//retorna codigo 200 OK
+        }
 
-		[HttpPost]
-		public IActionResult Create([FromBody] Telefones loja)
-		{
-			if (loja == null)
-				return BadRequest();
+        [HttpPost]
+        public IActionResult Create([FromBody] Telefones loja)
+        {
+            if (loja == null)
+                return BadRequest();
 
-			_telefoneRepositorio.Add(loja);
-			//return Json(loja);
-			return CreatedAtRoute("GetLoja", new Telefones { IdUser = loja.IdUser }, loja);
-		}
+            _telefoneRepositorio.Add(loja);
+            //return Json(loja);
+            return CreatedAtRoute("GetLoja", new Telefones { IdUser = loja.IdUser }, loja);
+        }
 
-		[HttpPut("{id}")]
-		public IActionResult Update(int id, [FromBody] Telefones loja)
-		{
-			if (loja == null)
-			{
-				return BadRequest();
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Telefones loja)
+        {
+            if (loja == null)
+            {
+                return BadRequest();
 
-			}
-			var novaLoja = _telefoneRepositorio.Find(id);
+            }
+            var novaLoja = _telefoneRepositorio.Find(id);
 
-			if (novaLoja == null)
-			{
-				return BadRequest();
+            if (novaLoja == null)
+            {
+                return BadRequest();
 
-			}
-			novaLoja.Loja = loja.Loja;
-			novaLoja.Fone = loja.Fone;
-			novaLoja.Cell = loja.Cell;
-			_telefoneRepositorio.Update(novaLoja);
-			return Json(novaLoja);
+            }
+            novaLoja.Loja = loja.Loja;
+            novaLoja.Fone = loja.Fone;
+            novaLoja.Cell = loja.Cell;
+            _telefoneRepositorio.Update(novaLoja);
+            return Json(novaLoja);
 
-		}
-		// [HttpPut("{id}")]
-		// public IActionResult Update(long id, [FromBody] Telefones loja)
-		// {
-		// 	if (loja == null || loja.iduser != id)
-		// 		return BadRequest();
+        }
 
-		// 	var Contatoloja = _telefoneRepositorio.Find(id);
+        // [HttpPut("{id}")]
+        // public IActionResult Update(long id, [FromBody] Telefones loja)
+        // {
+        // 	if (loja == null || loja.iduser != id)
+        // 		return BadRequest();
 
-
-		// 	if (loja == null)
-		// 		return NotFound();
-
-		// 	_telefoneRepositorio.
+        // 	var Contatoloja = _telefoneRepositorio.Find(id);
 
 
-		// 	_telefoneRepositorio.Update(Contatoloja);
-		// 	return new NoContentResult();
+        // 	if (loja == null)
+        // 		return NotFound();
 
-		// }
-
-		// [HttpDelete("{id}")]
-		// public IActionResult Delete(long id)
-		// {
-		// 	var Contatoloja = _telefoneRepositorio.Find(id);
-		// 	if (loja == null)
-		// 		return NotFound;
-
-		// 	_telefoneRepositorio.Remove(id);
-		// 	return new NoContentResult();
-		// }
+        // 	_telefoneRepositorio.
 
 
-	}
+        // 	_telefoneRepositorio.Update(Contatoloja);
+        // 	return new NoContentResult();
+
+        // }
+
+        // [HttpDelete("{id}")]
+        // public IActionResult Delete(long id)
+        // {
+        // 	var Contatoloja = _telefoneRepositorio.Find(id);
+        // 	if (loja == null)
+        // 		return NotFound;
+
+        // 	_telefoneRepositorio.Remove(id);
+        // 	return new NoContentResult();
+        // }
+
+
+    }
 }
